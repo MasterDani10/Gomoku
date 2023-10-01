c5c97496dca9be9e5790be2a5a95efdc97998fa1
Benjamin Laffita <benjaminlaffita@gmail.com>
2023-09-30 19:39:50 -0600

    Adding classes for the Game

--- src/GomokuGame/Gomoku.java
+++ src/GomokuGame/Gomoku.java
@@ -1,3 +1,5 @@
+package GomokuGame;
+
 public class Gomoku {
     public static void main(String[] args){
         System.out.println("Checking");
---
+++ src/GomokuGame/LeaderBoard.java
@@ -0,0 +1,4 @@
+package GomokuGame;
+
+public class LeaderBoard {
+}
