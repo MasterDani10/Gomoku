75e05feab5c4f089ed0db4f8e264325105b92873
MasterDani10 <143663112+MasterDani10@users.noreply.github.com>
2023-10-02 15:25:05 -0600

    Created Board class

---
+++ src/Board.java
@@ -0,0 +1,49 @@
+public class Board {
+    public static void main(String[] args){
+        int n = 14;
+        int m = 15;
+        int[][] board = new int[m][n];
+
+
+        System.out.println();
+        for(int x = 1; x <= n+1; x++){
+            if(x == 1){
+                System.out.print("      " + x + " ");
+            }
+            else if(x == n+1){
+                System.out.println(" " + x);
+            }
+            else if(x > 9){
+                System.out.print(" " + x + " ");
+            }
+            else {
+                System.out.print("  " + x + " ");
+            }
+        }
+
+        for(int i = 0; i < m; i++){
+            for(int j = 0; j < n; j++){
+                if(board[i][j] == 0){
+                    if(j == 0){
+                        if(i > 8){
+                            System.out.print(i + 1 + "    .   ");
+                        }
+                        else {
+                            System.out.print(i + 1 + "     .   ");
+                        }
+                    }
+                    if(j == n-1){
+                        System.out.println(".");
+                    }
+                    else {
+                        System.out.print(".   ");
+                    }
+                }
+
+            }
+        }
+
+
+
+    }
+}
